776191c428cb6fade0b4b1a7c5c498d097037d73
caianesantos <caianesantos.ph@gmail.com>
2025-11-16 17:57:38 -0300

    prova de funções 1 do módulo de python

---
+++ exercices/maiornum.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia 3 números 
+# e informe o maior número e o menor.
+
+numero = int(input([x for x in range(3) print(f"maior: {>} e menor: {<}")])) :
+
+for i in numero(3):
+    num1 = int(input("Digite {i}° número: "))
+    num2 = int(input("Digite {i}° número: "))  
+    num3 = int(input("Digite {i}° número: ")) 
+    
+    numeros = 
---
+++ python-module/proof04.py
@@ -0,0 +1,13 @@
+#Funções 1
+"""
+Crie uma função chamada media que receberá três números como argumentos.
+Esta função deve calcular a média aritmética desses três números.
+Para fazer isso, some os três números e, em seguida, divida o resultado por três.
+Por fim, a função deve retornar o valor da média aritmética calculada.
+"""
+
+def media(a,b,c):
+    calcular = (a + b + c)/3
+    return calcular
+
+print(media(1,2,3))
